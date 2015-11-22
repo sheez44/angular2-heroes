cb9c15355647215794e530472a6b8bc50a8687d5
sheez <blaat2k@mail.com>
2015-11-22 13:41:24 +0100

    Add Heroes array and add selection possibility

--- Github/food-angular/src/app/app.js
+++ Github/food-angular/src/app/app.js
@@ -6,25 +6,27 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
         case 4: return decorators.reduceRight(function(o, d) { return (d && d(target, key, o)) || o; }, desc);
     }
 };
-var __metadata = (this && this.__metadata) || function (k, v) {
-    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-};
 var angular2_1 = require('angular2/angular2');
 var AppComponent = (function () {
     function AppComponent() {
         this.title = 'Tour of Heroes';
-        this.hero = {
-            id: 1,
-            name: 'Paladin Danse'
-        };
+        this.heroes = HEROES;
     }
+    AppComponent.prototype.onSelect = function (hero) {
+        this.selectedHero = hero;
+    };
+    AppComponent.prototype.getSelectedClass = function (hero) {
+        return {
+            'selected': hero === this.selectedHero
+        };
+    };
     AppComponent = __decorate([
         angular2_1.Component({
             selector: 'food-menu',
-            template: "\n    <div class=\"container\">\n        <h1>{{title}} <span class=\"label label-default\">!!!</span></h1>\n        <h2>{{hero.name}} details!</h2>\n        <div><label>id:</label>{{hero.id}}</div>\n        <label for=\"name-editor\">name:</label>\n        <div class=\"input-group\">\n            <div><input id=\"name-editor\" type=\"text\" class=\"form-control\" [(ng-model)]=\"hero.name\" placeholder=\"name\"></div>\n        </div>\n    </div>\n    ",
-            directives: [angular2_1.FORM_DIRECTIVES]
-        }), 
-        __metadata('design:paramtypes', [])
+            template: "\n    <div class=\"container\">\n        <h1>{{title}} <span class=\"label label-default\">!!!</span></h1>\n                <h2>My Heroes</h2>\n        <ul class=\"heroes\">\n          <li *ng-for=\"#hero of heroes\"\n              [ng-class]=\"getSelectedClass(hero)\"\n              (click)=\"onSelect(hero)\">\n            <span class=\"badge\">{{hero.id}}</span>{{hero.name}}\n          </li>\n        </ul>\n        <div *ng-if=\"selectedHero\">\n            <h2>{{selectedHero.name}} details!</h2>\n            <div><label>id:</label>{{selectedHero.id}}</div>\n            <label for=\"name-editor\">name:</label>\n            <div class=\"input-group\">\n                <div><input id=\"name-editor\" type=\"text\" class=\"form-control\" [(ng-model)]=\"selectedHero.name\" placeholder=\"name\"></div>\n            </div>\n        </div>\n    </div>\n    ",
+            directives: [angular2_1.FORM_DIRECTIVES, angular2_1.NgFor, angular2_1.NgIf, angular2_1.NgClass],
+            styles: ["\n  .heroes {list-style-type: none; margin-left: 1em; padding: 0; width: 10em;}\n  .heroes li { cursor: pointer; position: relative; left: 0; transition: all 0.2s ease; }\n  .heroes li:hover {color: #369; background-color: #EEE; left: .2em;}\n  .heroes .badge {\n    font-size: small;\n    color: white;\n    padding: 0.1em 0.7em;\n    background-color: #369;\n    line-height: 1em;\n    position: relative;\n    left: -1px;\n    top: -1px;\n    margin-right: 1em;\n  }\n  .selected { background-color: #EEE; color: #369; }\n  "]
+        })
     ], AppComponent);
     return AppComponent;
 })();
@@ -33,5 +35,17 @@ var Hero = (function () {
     }
     return Hero;
 })();
+var HEROES = [
+    { "id": 11, "name": "Mr. Nice" },
+    { "id": 12, "name": "Narco" },
+    { "id": 13, "name": "Bombasto" },
+    { "id": 14, "name": "Celeritas" },
+    { "id": 15, "name": "Magneta" },
+    { "id": 16, "name": "RubberMan" },
+    { "id": 17, "name": "Dynama" },
+    { "id": 18, "name": "Dr IQ" },
+    { "id": 19, "name": "Magma" },
+    { "id": 20, "name": "Tornado" }
+];
 angular2_1.bootstrap(AppComponent);
 //# sourceMappingURL=app.js.map
--- Github/food-angular/src/app/app.ts
+++ Github/food-angular/src/app/app.ts
@@ -1,26 +1,61 @@
-import {bootstrap, Component, FORM_DIRECTIVES} from 'angular2/angular2';
+import {bootstrap, Component, FORM_DIRECTIVES, NgFor, NgIf, NgClass} from 'angular2/angular2';
 
 @Component({
     selector: 'food-menu',
     template: `
     <div class="container">
         <h1>{{title}} <span class="label label-default">!!!</span></h1>
-        <h2>{{hero.name}} details!</h2>
-        <div><label>id:</label>{{hero.id}}</div>
-        <label for="name-editor">name:</label>
-        <div class="input-group">
-            <div><input id="name-editor" type="text" class="form-control" [(ng-model)]="hero.name" placeholder="name"></div>
+                <h2>My Heroes</h2>
+        <ul class="heroes">
+          <li *ng-for="#hero of heroes"
+              [ng-class]="getSelectedClass(hero)"
+              (click)="onSelect(hero)">
+            <span class="badge">{{hero.id}}</span>{{hero.name}}
+          </li>
+        </ul>
+        <div *ng-if="selectedHero">
+            <h2>{{selectedHero.name}} details!</h2>
+            <div><label>id:</label>{{selectedHero.id}}</div>
+            <label for="name-editor">name:</label>
+            <div class="input-group">
+                <div><input id="name-editor" type="text" class="form-control" [(ng-model)]="selectedHero.name" placeholder="name"></div>
+            </div>
         </div>
     </div>
     `,
-    directives: [FORM_DIRECTIVES]
+    directives: [FORM_DIRECTIVES, NgFor, NgIf, NgClass],
+    styles:[`
+  .heroes {list-style-type: none; margin-left: 1em; padding: 0; width: 10em;}
+  .heroes li { cursor: pointer; position: relative; left: 0; transition: all 0.2s ease; }
+  .heroes li:hover {color: #369; background-color: #EEE; left: .2em;}
+  .heroes .badge {
+    font-size: small;
+    color: white;
+    padding: 0.1em 0.7em;
+    background-color: #369;
+    line-height: 1em;
+    position: relative;
+    left: -1px;
+    top: -1px;
+    margin-right: 1em;
+  }
+  .selected { background-color: #EEE; color: #369; }
+  `]
 })
 
 class AppComponent {
     public title = 'Tour of Heroes';
-    public hero: Hero = {
-        id: 1,
-        name: 'Paladin Danse'
+    public selectedHero: Hero;
+    public heroes = HEROES;
+
+    onSelect(hero: Hero) {
+        this.selectedHero = hero;
+    }
+
+    getSelectedClass(hero: Hero) {
+        return {
+            'selected': hero === this.selectedHero
+        };
     }
 }
 
@@ -28,5 +63,20 @@ class Hero {
     id: number;
     name: string;
 }
+
+var HEROES: Hero[] = [
+    { "id": 11, "name": "Mr. Nice" },
+    { "id": 12, "name": "Narco" },
+    { "id": 13, "name": "Bombasto" },
+    { "id": 14, "name": "Celeritas" },
+    { "id": 15, "name": "Magneta" },
+    { "id": 16, "name": "RubberMan" },
+    { "id": 17, "name": "Dynama" },
+    { "id": 18, "name": "Dr IQ" },
+    { "id": 19, "name": "Magma" },
+    { "id": 20, "name": "Tornado" }
+];
+
+
 
 bootstrap(AppComponent);
